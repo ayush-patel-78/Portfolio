a99c5b81b7813a062a18b87d43a56b7d2a3b0d0b
ayush-patel-78 <79853695+ayush-patel-78@users.noreply.github.com>
2021-04-09 04:18:43 +0530

    changes in message

--- js/app.js
+++ js/app.js
@@ -15,8 +15,26 @@ $(document).ready(function(){
         mobileNav.classList.remove('open');
 
     });
-    mobileNav.about.addEventListener('click', function(){
-        mobileNav.classlist.remove('open');
-    });
-
+  
 });
+
+var form = document.getElementById("my-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    status.innerHTML = "Thanks for your submission!";
+    form.reset()
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
